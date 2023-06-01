fd0a90060b0b7dcab3e85e05838d8dcab8762b2e
anu-baby97 <anubbey@gmail.com>
2023-06-01 15:00:28 +0530

    First commit

---
+++ QALegendAutomation/src/test/java/reTry/Retry.java
@@ -0,0 +1,22 @@
+package reTry;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+import org.testng.annotations.Test;
+
+public class Retry implements IRetryAnalyzer {
+	int count=0;
+	int retryLimit=2;
+
+
+@Override
+public boolean retry(ITestResult result) {
+	
+	if(count<retryLimit) {
+		count ++;
+		return true;
+	}
+	// TODO Auto-generated method stub
+	return false;
+}
+}
